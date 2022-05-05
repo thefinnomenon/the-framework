2d4cd5b5e6707708e4c918dd5a0d056807533783
Chris Finn <chris@finnternet.com>
2022-05-05 09:12:31 -0300

    ci: cicd #1 add githooks (#2)
    * feat: initial setup
    
    * feat: local env vars
    
    * chore: forbid commit to main with githook
    
    * feat: add branch naming convention hook
    
    * feat: add branch naming convention hook
    
    * chore: add max branch name length check
    
    * feat: add branch naming policy enforcement hook
    
    * fix: allow # in branch name
    
    * chore: Add husky. Add commit-msg hook to append issue number from branch.
    
    * chore: cleanup
    
    * ci: add semantic PR check github action
    
    Closes issue #1

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      development: {
+        plugins: [["inline-dotenv", {
+          path: './envs/.env'
+        }]]
+      },
+      development: {
+        plugins: [["inline-dotenv", {
+          path: './envs/.env'
+        }]]
+      },
+    }
   };
 };
